25e2c1313f2674ea02057ccb82bca1d989993ee7
Antoine <apichard@gaming.tech>
2024-03-19 18:39:09 +0100

    init eventmanager

--- TurnPerTurn/Entity.cs
+++ TurnPerTurn/Entity.cs
@@ -5,15 +5,21 @@ public class Entity
     public int Speed;
     public int Evade;
 
+    public event Action OnTakeDamage;
+    public event Action OnDeath;
+
+
     public void TakeDamage(int amount)
     {
         Hp -= amount;
+        OnTakeDamage.Invoke();
     }
 
     public bool IsDead()
     {
         if (Hp <= 0) //dead
         {
+            OnDeath?.Invoke(); //previent toutes les classes concernés
             Speed = 0;
             Damage = 0;
             Evade = 0;
@@ -23,5 +29,9 @@ public class Entity
         {
             return false;
         } 
+    }
+    public void UpadateSlider()
+    {
+        Console.WriteLine("oui");
     }
 }
---
+++ TurnPerTurn/EventManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class EventManager
+{
+    public static event Action OnTakeDamage;
+    public static event Action OnDeath;
+}
+
